84f6404e394928fa5df9d33e170ca53de2907255
hazemkammoun <hazem.kammoun@esprit.tn>
2025-02-11 11:36:39 +0100

    first commit

---
+++ src/main/java/com/esprit/firstspringbootproject/entities/Foyer.java
@@ -0,0 +1,26 @@
+package com.esprit.firstspringbootproject.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Foyer implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idFoyer;
+    private String nomFoyer;
+    private long capaciteFoyer;
+
+}
---
+++ src/main/java/com/esprit/firstspringbootproject/entities/Reservation.java
@@ -0,0 +1,23 @@
+package com.esprit.firstspringbootproject.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation implements Serializable {
+    @Id
+    private String idReservation;
+    @Temporal(TemporalType.DATE)
+    private Date anneeUniversitaire;
+    private boolean estvalide;
+}
