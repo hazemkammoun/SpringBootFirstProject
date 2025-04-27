7b763ff41ed752f92f4c652ed99a01448cf7e4e0
hazemkammoun <hazem.kammoun@esprit.tn>
2025-04-27 13:15:03 +0100

    mise à jour

--- src/main/java/com/esprit/firstspringbootproject/FirstSpringBootProjectApplication.java
+++ src/main/java/com/esprit/firstspringbootproject/FirstSpringBootProjectApplication.java
@@ -3,9 +3,12 @@ package com.esprit.firstspringbootproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableScheduling
 public class FirstSpringBootProjectApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/esprit/firstspringbootproject/config/PerformanceAspect.java
@@ -0,0 +1,22 @@
+package com.esprit.firstspringbootproject.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class PerformanceAspect {
+
+    @Around("execution(* com.esprit..firstspringbootproject.services.*.*(..))")
+    public Object profile(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object obj = pjp.proceed();
+        long elapsedTime = System.currentTimeMillis() - start;
+        log.info("Method execution time: " + elapsedTime + " milliseconds.");
+        return obj;
+    }
+}
